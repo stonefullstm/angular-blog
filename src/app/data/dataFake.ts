decd31a9fc2ea3f827fa4be1aa70eb3ac9367975
stonefullstm <stonefull.stm@gmail.com>
2023-10-16 17:20:35 -0300

    refactor: Alterações para conteúdo dinâmico nas páginas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photoCover": "https://picsum.photos/500/400",
+    "title": "Minha Notícia",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi obcaecati doloribus illum libero, magnam ipsum incidunt ducimus velit doloremque hic unde ex. Recusandae architecto eius non necessitatibus eligendi illum blanditiis!"
+  },
+  {
+    "id": "2",
+    "photoCover": "https://picsum.photos/200/100",
+    "title": "Lorem ipsum dolor sit amet",
+    "description": "bla bla bla"
+  },
+  {
+    "id": "3",
+    "photoCover": "https://picsum.photos/201/100",
+    "title": "Consectetur adipiscing elit",
+    "description": "bla bla bla bla bla"
+  },
+  {
+    "id": "4",
+    "photoCover": "https://picsum.photos/202/100",
+    "title": "Praesent ac nunc ante",
+    "description": "bla bla bla bla bla bla bla bla"
+  }
+];
